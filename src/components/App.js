89f1e3c9f29a902632b14db8bbddd9784eb5fe10
king0120 <king0120@gmail.com>
2017-07-19 12:17:43 -0400

    prepping starter code

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img className="backgroundImg" src="http://i.imgur.com/DysM91b.png"/>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
